5775819bf8511db6c8770f19398fa725d7d56524
qiqingfu <9116895@qq.com>
2020-07-29 17:56:22 +0800

    feat(node/cache): http 协商缓存 Last-Modified 服务端实现

--- node/cache/index.js
+++ node/cache/index.js
@@ -22,24 +22,66 @@ function onRequest(req, res) {
       const filePath = path.resolve(staticDirPath, "index.html")
       content = fs.readFileSync(filePath)
       type = mime.getType(filePath)
+      setHeader(res, content, () => {
+        res.setHeader("Content-Type", `${type}; charset=utf8`)
+      })
     } else {
+      let modified = req.headers["if-modified-since"]
+      let etag = req.headers['etag']
       const filePath = path.join(staticDirPath, pathname)
-      content = fs.readFileSync(filePath)
-      type = mime.getType(filePath)
-    }
+      fs.stat(filePath, function (err, stats) {
+        if (err) {
+          throw new Error(err.message)
+        }
 
-    // 缓存 5s
-    res.setHeader("Cache-Control", "max-age=5")
-    // res.setHeader("Pragma", "no-cache")
-    // 缓存 10s
-    res.setHeader("Expires", new Date(Date.now() + 1000 * 10).toGMTString())
-    res.setHeader("Content-Type", `${type}; charset=utf8`)
-    res.end(content)
+        if (etag) {
+
+        }
+
+        let mtime = toUTCTime(stats.mtime)
+        /**
+         * 协商缓存, 如果客户端缓存资源依然新鲜, 服务器资源未发生改变, 则使用客户端缓存
+         */
+        if (modified === mtime) {
+          res.statusCode = 304
+          setHeader(res,  () => {
+            res.setHeader("Content-Type", `${type}; charset=utf8`)
+            res.setHeader("Last-Modified", toUTCTime(mtime))
+          })
+          res.end()
+        } else {
+          content = fs.readFileSync(filePath)
+          type = mime.getType(filePath)
+          setHeader(res, content, () => {
+            res.setHeader("Content-Type", `${type}; charset=utf8`)
+            res.setHeader("Last-Modified", toUTCTime(mtime))
+          })
+        }
+      })
+    }
   } else {
     res.end(`
       not supper ${pathname} : ${req.method}
     `)
   }
+}
+
+function setHeader(res, content, fn) {
+  if (typeof content === "function") {
+    fn = content
+    content = ""
+  }
+  // 缓存 5s
+  res.setHeader("Cache-Control", "no-cache")
+  // res.setHeader("Pragma", "no-cache")
+  // 缓存 10s
+  // res.setHeader("Expires", new Date(Date.now() + 1000 * 10).toGMTString())
+  fn()
+  res.end(content)
+}
+
+function toUTCTime(time) {
+  return new Date(time).toUTCString()
 }
 
 server.listen(3001)
