d10615b24411e805cc99576f310cb374e0e0bb5b
qiqingfu <9116895@qq.com>
2020-08-13 17:15:23 +0800

    feat(node/egg-tcp): 新增 client 和 server 通过 net 通讯

---
+++ node/egg-tcp/app/tcp/controller/index.js
@@ -0,0 +1,30 @@
+/**
+ * Created by qiqf on 2020/8/13
+ */
+module.exports = app => {
+  return {
+    async feed (socket) {
+      console.log('A new connection has been established.');
+
+      // Now that a TCP connection has been established, the server can send data to
+      // the client by writing to its socket.
+      socket.write('Hello, client.');
+
+      // The server can also receive data from the client by reading from its socket.
+      socket.on('data', function (chunk) {
+        console.log(`Data received from client: ${chunk.toString()}.`);
+      });
+
+      // When the client requests to end the TCP connection with the server, the server
+      // ends the connection.
+      socket.on('end', function () {
+        console.log('Closing connection with the client');
+      });
+
+      // Don't forget to catch error, for your own sake.
+      socket.on('error', function (err) {
+        console.log(`Error: ${err}`);
+      });
+    }
+  }
+}
