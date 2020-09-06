bb8bfacd1b4b443e60aa00e88fa78ca7d4acb46f
qiqf <723469354@qq.com>
2020-09-06 11:09:52 +0800

    style(cache): 优化部分代码和格式化

--- node/cache/index.js
+++ node/cache/index.js
@@ -1,76 +1,76 @@
 /**
  * Created by qiqf on 2020/7/29
  */
-const http = require("http")
-const path = require("path")
-const fs = require("fs")
-const url = require("url")
-const mime = require("mime")
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const url = require('url');
+const mime = require('mime');
 
 /**
  * 是否开启协商缓存
  * @type {boolean}
  */
-const IS_NEGOTIATE_CACHE = false
+const IS_NEGOTIATE_CACHE = true;
 /**
  * 缓存的类型
  * @type {{modified: boolean, etag: boolean}}
  */
 const NEGOTIATE_CACHE_TYPES = {
-  'modified': false,
-  'etag': false
-}
+  modified: true,
+  etag: false,
+};
 
-const server = http.createServer(onRequest)
+const server = http.createServer(onRequest);
 
 function onRequest(req, res) {
-  let {pathname} = url.parse(req.url)
+  let { pathname } = url.parse(req.url);
 
-  if ("/favicon.ico" === pathname) return
+  if ('/favicon.ico' === pathname) return;
 
-  const staticDirPath = path.resolve(__dirname, "public")
+  const staticDirPath = path.resolve(__dirname, 'public');
 
-  if ("GET" === req.method || "HEAD" === req.method) {
-    let content, type
-    if ("/" === pathname) {
-      const filePath = path.resolve(staticDirPath, "index.html")
-      content = fs.readFileSync(filePath)
-      type = mime.getType(filePath)
+  if ('GET' === req.method || 'HEAD' === req.method) {
+    let content, type;
+    if ('/' === pathname) {
+      const filePath = path.resolve(staticDirPath, 'index.html');
+      content = fs.readFileSync(filePath);
+      type = mime.getType(filePath);
       setHeader(res, content, () => {
-        res.setHeader("Content-Type", `${type}; charset=utf8`)
-      })
+        res.setHeader('Content-Type', `${type}; charset=utf8`);
+      });
     } else {
-      const filePath = path.join(staticDirPath, pathname)
+      const filePath = path.join(staticDirPath, pathname);
       /**
        * 在不开启协商缓存的情况
        */
       if (!IS_NEGOTIATE_CACHE) {
-        content = fs.readFileSync(filePath)
-        type = mime.getType(filePath)
+        content = fs.readFileSync(filePath);
+        type = mime.getType(filePath);
         return setHeader(res, content, () => {
-          res.setHeader("Content-Type", `${type}; charset=utf8`)
-        })
+          res.setHeader('Content-Type', `${type}; charset=utf8`);
+        });
       }
 
-      send(req, res, filePath)
+      send(req, res, filePath);
     }
   } else {
     res.end(`
       not supper ${pathname} : ${req.method}
-    `)
+    `);
   }
 }
 
 function setHeader(res, content, fn) {
-  if (typeof content === "function") {
-    fn = content
-    content = ""
+  if (typeof content === 'function') {
+    fn = content;
+    content = '';
   }
-  // res.setHeader("Pragma", "no-cache")
-  // res.setHeader("Expires", new Date(Date.now() + 1000 * 60 * 60 * 24 * 7).toGMTString())
-  res.setHeader("Cache-Control", "max-age=10")
-  fn && fn()
-  res.end(content)
+  res.setHeader('Pragma', 'no-cache');
+  // res.setHeader("Expires", 0)
+  res.setHeader('Cache-Control', 'max-age=0');
+  fn && fn();
+  res.end(content);
 }
 
 /**
@@ -79,7 +79,7 @@ function setHeader(res, content, fn) {
  * @returns {string}
  */
 function toUTCTime(time) {
-  return new Date(time).toUTCString()
+  return new Date(time).toUTCString();
 }
 
 /**
@@ -87,58 +87,58 @@ function toUTCTime(time) {
  * @param stat
  */
 function statTag(stat) {
-  const mtime = stat.mtime.getTime().toString(16)
-  const size = stat.size.toString(16)
+  const mtime = stat.mtime.getTime().toString(16);
+  const size = stat.size.toString(16);
 
-  return '"' + size + '-' + mtime + '"'
+  return '"' + size + '-' + mtime + '"';
 }
 
 function send(req, res, filePath) {
-  let modified = req.headers["if-modified-since"]
-  let etag = req.headers['if-none-match']
-  let type, mtime, content, weak
+  let modified = req.headers['if-modified-since'];
+  let etag = req.headers['if-none-match'];
+  let type, mtime, content, weak;
 
   fs.stat(filePath, function (err, stats) {
     if (err) {
-      throw new Error(err.message)
+      throw new Error(err.message);
     }
 
-    type = mime.getType(filePath)
-    mtime = toUTCTime(stats.mtime)
-    res.setHeader("Content-Type", `${type}; charset=utf8`)
-    weak = 'W/' + statTag(stats)
+    type = mime.getType(filePath);
+    mtime = toUTCTime(stats.mtime);
+    res.setHeader('Content-Type', `${type}; charset=utf8`);
+    weak = 'W/' + statTag(stats);
 
     /**
      * 命中 Etag 协商缓存, 服务器资源为发生改变, 则使用客户端缓存
      */
     if (etag && etag === weak) {
-      res.statusCode = 304
+      res.statusCode = 304;
       return setHeader(res, () => {
-        res.setHeader("ETag", weak)
-      })
+        res.setHeader('ETag', weak);
+      });
     }
 
     /**
      * 协商缓存, 如果客户端缓存资源依然新鲜, 服务器资源未发生改变, 则使用客户端缓存
      */
     if (modified === mtime) {
-      res.statusCode = 304
+      res.statusCode = 304;
       return setHeader(res, () => {
-        res.setHeader("Last-Modified", toUTCTime(mtime))
-      })
+        res.setHeader('Last-Modified', toUTCTime(mtime));
+      });
     }
 
     if (NEGOTIATE_CACHE_TYPES.etag) {
-      res.setHeader("ETag", weak)
+      res.setHeader('ETag', weak);
     }
 
     if (NEGOTIATE_CACHE_TYPES.modified) {
-      res.setHeader("Last-Modified", toUTCTime(mtime))
+      res.setHeader('Last-Modified', toUTCTime(mtime));
     }
 
-    content = fs.readFileSync(filePath)
-    setHeader(res, content)
-  })
+    content = fs.readFileSync(filePath);
+    setHeader(res, content);
+  });
 }
 
-server.listen(3001)
+server.listen(3001);
